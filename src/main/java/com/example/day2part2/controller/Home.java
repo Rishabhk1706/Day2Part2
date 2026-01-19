77e1eb3190267406bbabc705fa8113090dffa7bf
Rishabhk1706 <rishabhk1706@gmail.com>
2026-01-19 15:16:57 +0530

    displayed 5 students

--- src/main/java/com/example/day2part2/controller/Home.java
+++ src/main/java/com/example/day2part2/controller/Home.java
@@ -1,15 +1,21 @@
 package com.example.day2part2.controller;
-import java.util.*;
 import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student = new StudentModel(1,"Rishabh","rk@gmail.com");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> students = new ArrayList<>();
+        students.add(new StudentModel(1, "Rishabh", "rk1@gmail.com"));
+        students.add(new StudentModel(2, "Aman", "aman@gmail.com"));
+        students.add(new StudentModel(3, "Neha", "neha@gmail.com"));
+        students.add(new StudentModel(4, "Priya", "priya@gmail.com"));
+        students.add(new StudentModel(5, "Rahul", "rahul@gmail.com"));
+        return students;
     }
     @GetMapping("/about")
     public String about(){
