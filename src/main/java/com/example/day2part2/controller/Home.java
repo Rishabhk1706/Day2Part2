814cdf3aa0a820e5a1fcc111fbb2490eb45cde65
Rishabhk1706 <rishabhk1706@gmail.com>
2026-01-19 15:09:04 +0530

    created first api

--- src/main/java/com/example/day2part2/controller/Home.java
+++ src/main/java/com/example/day2part2/controller/Home.java
@@ -1,10 +1,16 @@
 package com.example.day2part2.controller;
-
+import java.util.*;
+import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
+    @GetMapping("/")
+    public StudentModel getStudent(){
+        StudentModel student = new StudentModel(1,"Rishabh","rk@gmail.com");
+        return student;
+    }
     @GetMapping("/about")
     public String about(){
         return "This is the about page";
